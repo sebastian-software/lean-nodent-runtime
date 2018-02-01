d28f5122c4eb35d54379a9b94283bfdc5c38eabb
Sebastian Werner <s.werner@sebastian-software.de>
2018-02-01 20:47:28 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+function $asyncbind(self,catcher) {
+  "use strict";
+  var resolver = this;
+  switch (catcher) {
+  case true:
+  case 0:
+      return new ($asyncbind.Promise)(self);
+  case undefined:
+      return function boundThen() {
+          return resolver.apply(self,arguments);
+      }
+  default:
+      return function(){
+          try {
+              return resolver.apply(self,arguments);
+          } catch(ex) {
+              return catcher(ex);
+          }
+      }
+  }
+};
+
+function trampoline(t,x,s,e,u){
+  return function b(q) {
+      while (q) {
+          if (q.then) {
+              q = q.then(b, e) ;
+              return u?undefined:q;
+          }
+          try {
+              if (q.pop) {
+                  if (q.length)
+                    return q.pop() ? x.call(t) : q;
+                  q = s;
+               } else
+                  q = q.call(t)
+          } catch (r) {
+              return e(r);
+          }
+      }
+  }
+};
+
+$asyncbind.Promise = Promise;
+$asyncbind.trampoline = trampoline;
