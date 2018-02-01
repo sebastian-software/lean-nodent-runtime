6d4ef012abf0cd26cf5658560c263a961d8f9872
Sebastian Werner <s.werner@sebastian-software.de>
2018-02-01 20:48:33 +0100

    Code formatting

--- src/index.js
+++ src/index.js
@@ -1,45 +1,51 @@
-function $asyncbind(self,catcher) {
-  "use strict";
-  var resolver = this;
+function $asyncbind(self, catcher) {
+  const resolver = this
   switch (catcher) {
-  case true:
-  case 0:
-      return new ($asyncbind.Promise)(self);
-  case undefined:
+    case true:
+    case 0:
+      return new $asyncbind.Promise(self)
+    case undefined:
       return function boundThen() {
-          return resolver.apply(self,arguments);
+        return resolver.apply(self, arguments)
       }
-  default:
-      return function(){
-          try {
-              return resolver.apply(self,arguments);
-          } catch(ex) {
-              return catcher(ex);
-          }
+    default:
+      return function() {
+        try {
+          return resolver.apply(self, arguments)
+        } catch (ex) {
+          return catcher(ex)
+        }
       }
   }
-};
+}
 
-function trampoline(t,x,s,e,u){
+function trampoline(t, x, s, e, u) {
   return function b(q) {
-      while (q) {
-          if (q.then) {
-              q = q.then(b, e) ;
-              return u?undefined:q;
-          }
-          try {
-              if (q.pop) {
-                  if (q.length)
-                    return q.pop() ? x.call(t) : q;
-                  q = s;
-               } else
-                  q = q.call(t)
-          } catch (r) {
-              return e(r);
-          }
+    while (q) {
+      if (q.then) {
+        q = q.then(b, e)
+        return u ? undefined : q
       }
+      try {
+        if (q.pop) {
+          if (q.length) return q.pop() ? x.call(t) : q
+          q = s
+        } else q = q.call(t)
+      } catch (r) {
+        return e(r)
+      }
+    }
   }
-};
+}
 
-$asyncbind.Promise = Promise;
-$asyncbind.trampoline = trampoline;
+$asyncbind.Promise = Promise
+$asyncbind.trampoline = trampoline
+
+if (!Function.prototype.$asyncbind) {
+  Object.defineProperty(Function.prototype, "$asyncbind", {
+    value: $asyncbind,
+    enumerable: false,
+    configurable: true,
+    writable: true
+  })
+}
