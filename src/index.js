6e560df66f235d9ca8b8d0f61aae5f13d420214d
Sebastian Werner <s.werner@sebastian-software.de>
2018-02-01 20:52:15 +0100

    Linting fixes

--- src/index.js
+++ src/index.js
@@ -3,43 +3,21 @@ function $asyncbind(self, catcher) {
   switch (catcher) {
     case true:
     case 0:
-      return new $asyncbind.Promise(self)
+      return new Promise(self)
     case undefined:
       return function boundThen() {
         return resolver.apply(self, arguments)
       }
     default:
-      return function() {
+      return function inner() {
         try {
           return resolver.apply(self, arguments)
-        } catch (ex) {
-          return catcher(ex)
+        } catch (except) {
+          return catcher(except)
         }
       }
   }
 }
-
-function trampoline(t, x, s, e, u) {
-  return function b(q) {
-    while (q) {
-      if (q.then) {
-        q = q.then(b, e)
-        return u ? undefined : q
-      }
-      try {
-        if (q.pop) {
-          if (q.length) return q.pop() ? x.call(t) : q
-          q = s
-        } else q = q.call(t)
-      } catch (r) {
-        return e(r)
-      }
-    }
-  }
-}
-
-$asyncbind.Promise = Promise
-$asyncbind.trampoline = trampoline
 
 if (!Function.prototype.$asyncbind) {
   Object.defineProperty(Function.prototype, "$asyncbind", {
